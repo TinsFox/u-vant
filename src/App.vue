1619b2a2b0e4efaccef65099f472bcbd69ace9cf
TinsFox <1414849373@qq.com>
2021-11-11 16:37:02 +0800

    feat: config prettier

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@ import Vue from 'vue';
 export default Vue.extend({
   mpType: 'app',
   onLaunch() {
-    console.log('App Launch')
+    console.log('App Launch');
   },
   onShow() {
-    console.log('App Show')
+    console.log('App Show');
   },
   onHide() {
-    console.log('App Hide')
-  }
+    console.log('App Hide');
+  },
 });
 </script>
 
